aa101c41c466b690ce1d8b246ab0145cf810a5bb
lunaesomnia <lunaesomnia@protonmail.com>
2024-03-07 17:15:55 +0100

    init

---
+++ src/main/java/tds/App.java
@@ -0,0 +1,16 @@
+package tds;
+
+import javax.swing.JFrame;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        JFrame miFrame = new JFrame("Hola Mundo con Swing");
+        miFrame.setSize(300, 300);
+        miFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        miFrame.setVisible(true);
+    }
+}
